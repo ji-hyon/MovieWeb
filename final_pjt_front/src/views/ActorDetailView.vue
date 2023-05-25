7d8b2f887f5189ebd6e90878dd36276287d4c4af
ji-hyon <gyg03134@gmail.com>
2023-05-25 22:03:42 +0900

    ActorPage css

--- final_pjt_front/src/views/ActorDetailView.vue
+++ final_pjt_front/src/views/ActorDetailView.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
-    <h2>{{ actor.name }}</h2>
-    
-    <div v-if="error">{{ error }}</div>
-    <div v-else>
-      <img :src="getActorProfilePath(actor.profile_path)" :alt="actor.name" />
-      <ul>
-        <li v-for="movie in movies" :key="movie.id">
-          {{ movie.title }}
-        </li>
-      </ul>
+
+    <div class="actor-area">
+    <!-- <div> -->
+      <h2><i class="bi bi-person-heart"></i>&nbsp;&nbsp;{{ actor.name }}</h2>
+      
+      <div v-if="error">{{ error }}</div>
+      <div v-else>
+        <div class="actor">
+          <div class="test">
+          <img class="actor-img" :src="getActorProfilePath(actor.profile_path)" :alt="actor.name" />
+          </div>
+        </div>
+      <!-- </div> -->
     </div>
+    </div>
+
+      <br><br>
+      <div class="movies">
+        <h2 class="m"><i class="bi-film"></i>&nbsp; Movies</h2>
+        <div class="image-container">
+          <div v-for="movie in movies" :key="movie.id">
+            <p class="title">{{ movie.title }}</p>
+            <img class="img" :src="'https://image.tmdb.org/t/p/w500' + movie.poster_path">
+          </div>
+       </div>
+    </div>
+
   </div>
 </template>
 
@@ -63,10 +79,82 @@ export default {
 </script>
 
 <style scoped>
+.m {
+  padding-top: 60px;
+  padding-bottom: 40px;
+}
+
+.actor-area{
+  margin-top: 30px;
+  padding: 40px;
+  box-shadow: 0 20px 50px rgba(255, 127, 0, 0.3);
+}
+
+.movies{
+  box-shadow: 0 20px 50px rgba(255, 127, 0, 0.3);
+  margin-bottom: 100px;
+}
+
 h2 {
-    margin-top: 40px;
     color: rgb(153, 78, 153);
     font-family: 'Black Han Sans', sans-serif;
     padding: 15px;
 }
+.title {
+  font-weight: bold;
+  color: rgb(153, 78, 153);
+}
+
+
+.img {
+  width: 200px;
+}
+
+.image-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.actor{
+  display: flex;
+  justify-content: center;
+}
+
+.test {
+  width: 200px;
+  --borderWidth: 7px;
+  background: #e3c2e5;
+  position: relative;
+  border-radius: var(--borderWidth);
+  margin-bottom: 25px;
+}
+
+.test:after {
+  content: '';
+  position: absolute;
+  top: calc(-1 * var(--borderWidth));
+  left: calc(-1 * var(--borderWidth));
+  height: calc(100% + var(--borderWidth) * 2);
+  width: calc(100% + var(--borderWidth) * 2);
+  background: linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82);
+  border-radius: calc(2 * var(--borderWidth));
+  z-index: -1;
+  animation: animatedgradient 3s ease alternate infinite;
+  background-size: 300% 300%;
+}
+
+
+@keyframes animatedgradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
 </style>
